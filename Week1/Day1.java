5e02df78283eba5ead179f00968d91a8183209b0
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-09-19 23:13:13 +0530

    Create Day1.java

---
+++ Week1/Day1.java
@@ -0,0 +1,15 @@
+package Week1;
+public class Day1 {  // public class Java file - should be same as file name 
+    public static void main(String args[]){
+
+        //public - should be access to everyone
+        //static - methods can be run without creating object of Day1 class otherwise first I have to create Object like this Day1 obj = new Day1() and then run in main method but static is a good thing which says buddy dont make object of  class i will run for you 
+        //void - it means it doesn't return anything - just run the program
+        // main - java code start run from this in terminal 
+
+        System.out.println("Hello World ");
+        System.out.println("Hello I am CodeVibes");
+        System.out.println("Don't forget to Enjoy");
+
+    }
+}
